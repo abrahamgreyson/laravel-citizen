28815b0584513fede46640e06f30d161b421200e
abrahamgreyson <abrahamgreyson@gmail.com>
2025-02-07 14:33:52 +0800

    ✨ feat(functions): add service and publish functions

--- src/functions.php
+++ src/functions.php
@@ -1,10 +1,24 @@
 <?php
 
 if (! function_exists('service')) {
+    /**
+     *  取得服务实例
+     */
     function service($class)
     {
         $class = new \ReflectionClass($class);
 
+        return $class->getShortName();
+    }
+}
+
+if (! function_exists('publish')) {
+    /**
+     * 发布事件
+     */
+    function publish($class)
+    {
+        $class = new \ReflectionClass($class);
         return $class->getShortName();
     }
 }
